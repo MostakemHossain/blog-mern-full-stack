50fbc5008b0d06515a7e5796a600e32968bc583f
MostakemHossain <mostakememon123@gmail.com>
2024-01-30 16:43:00 +0600

    db connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 
-const app= express();
+dotenv.config();
+mongoose
+  .connect(
+    process.env.DB_URL
+  )
+  .then(() => {
+    console.log("DB connected");
+  }).catch((err)=>{
+    console.error(err);
+  });
 
-app.listen(5500,()=>{
-    console.log("Server is running on port 5500");
-})
+const app = express();
+
+app.listen(5500, () => {
+  console.log("Server is running on port 5500");
+});
