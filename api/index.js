9d00ff0833036e923add847c07d1133349dbb85f
MostakemHossain <mostakememon123@gmail.com>
2024-01-30 17:24:36 +0600

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import AuthRoutes from './routes/auth.route.js';
 import UserRoutes from "./routes/user.route.js";
 
 dotenv.config();
@@ -15,10 +16,12 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(5500, () => {
   console.log("Server is running on port 5500");
 });
 
 app.use('/api/user',UserRoutes);
+app.use('/api/auth',AuthRoutes);
 
