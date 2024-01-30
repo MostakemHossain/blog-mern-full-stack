91c3eaf43d136d820c770b83d44ff546a55825ad
MostakemHossain <mostakememon123@gmail.com>
2024-01-30 19:49:19 +0600

    add a error middleware

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,8 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
-export const singup = async (req, res) => {
+export const singup = async (req, res,next) => {
   const { username, email, password } = req.body;
 
   if (
@@ -12,7 +13,7 @@ export const singup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({ message: "Please fill all fields" });
+    next(errorHandler(400,"Please field all fields"))
   }
 
   const hashedPassword= bcryptjs.hashSync(password,10);
@@ -28,6 +29,6 @@ export const singup = async (req, res) => {
     message: "Sign up Successfull",
   });
   }catch(err){
-    return res.status(500).json({ message:err.message });
+    next(err)
   }
 };
