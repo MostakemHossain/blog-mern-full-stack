e7d05cfadd04751776c5356ceb954c6bd2e4c425
MostakemHossain <mostakememon123@gmail.com>
2024-01-30 17:01:52 +0600

    test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import UserRoutes from "./routes/user.route.js";
 
 dotenv.config();
 mongoose
@@ -18,3 +19,6 @@ const app = express();
 app.listen(5500, () => {
   console.log("Server is running on port 5500");
 });
+
+app.use('/api/user',UserRoutes);
+
