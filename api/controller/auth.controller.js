c2f2df9afd87a130c8c1bdae5f8705f1cb132f67
MostakemHossain <mostakememon123@gmail.com>
2024-01-31 20:12:53 +0600

    create sign in api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,8 +1,9 @@
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 
-export const singup = async (req, res,next) => {
+export const singup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
@@ -13,22 +14,51 @@ export const singup = async (req, res,next) => {
     email === "" ||
     password === ""
   ) {
-    next(errorHandler(400,"Please field all fields"))
+    next(errorHandler(400, "Please field all fields"));
   }
 
-  const hashedPassword= bcryptjs.hashSync(password,10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
   const newUser = new User({
     username,
     email,
-    password:hashedPassword,
+    password: hashedPassword,
   });
-  try{
+  try {
     await newUser.save();
-  res.json({
-    message: "Sign up Successfull",
-  });
-  }catch(err){
-    next(err)
+    res.json({
+      message: "Sign up Successfull",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "Please field all fields"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+     return next(errorHandler(404, "Invalid Email or Password"));
+    }
+    const hashedPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!hashedPassword) {
+     return next(errorHandler(400, "Invalid Email or Password"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SERECT);
+    const {password:pass,...rest}=validUser._doc
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+
+     
+  } catch (err) {
+    next(err);
   }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { singup } from "../controller/auth.controller.js";
+import { signin, singup } from "../controller/auth.controller.js";
 
 const router=express.Router(); 
 
 router.post('/signup',singup);
+router.post('/signin',signin);
 
 export default router;
