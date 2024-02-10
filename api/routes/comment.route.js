6fbbe1dcc22abfcc15e1e7d6f2c910cc081af14e
MostakemHossain <mostakememon123@gmail.com>
2024-02-11 00:26:10 +0600

    add like functiaonality to the comment components

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createComment, getPostComments } from "../controller/comment.controller.js";
+import { createComment, getPostComments, likeComment } from "../controller/comment.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router= express.Router();
 
 router.post('/create',verifyToken,createComment);
 router.get('/get-posts-comment/:postId',getPostComments);
+router.put('/like-comment/:commentId',verifyToken,likeComment);
 export default router;
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,57 +1,86 @@
 import { Alert, Button, Textarea } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Comment from "./Comment";
 
 export default function CommentSection({ postId }) {
+  const navigate = useNavigate();
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
   const [commentError, setCommentError] = useState("");
-  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if(comment.length>200) return;
+    if (comment.length > 200) return;
 
-    try{
-        const res = await  fetch("/api/comment/create", {
-            method: "POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({postId, content: comment,userId:currentUser._id})
-        });
-        const data= await res.json();
-        if(res.ok){
-            setComment('');
-            setCommentError(null)
-            setComments([data,...comments])
-        }
-    }catch(err){
-        setCommentError(err.message)
+    try {
+      const res = await fetch("/api/comment/create", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          postId,
+          content: comment,
+          userId: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      if (res.ok) {
+        setComment("");
+        setCommentError(null);
+        setComments([data, ...comments]);
+      }
+    } catch (err) {
+      setCommentError(err.message);
     }
-   
   };
 
-  useEffect(()=>{
-    const getComments= async ()=>{
-      try{
-        const res= await fetch(`/api/comment/get-posts-comment/${postId}`);
-        if(res.ok){
-          const data= await res.json();
-          
+  useEffect(() => {
+    const getComments = async () => {
+      try {
+        const res = await fetch(`/api/comment/get-posts-comment/${postId}`);
+        if (res.ok) {
+          const data = await res.json();
+
           setComments(data);
         }
-
-      }catch(err){
-        console.log(err.message)
+      } catch (err) {
+        console.log(err.message);
       }
-    }
+    };
     getComments();
+  }, [postId]);
 
-  },[postId])
+  const handleLike = async (commentId) => {
+    try {
+      if (!currentUser) {
+        navigate("/sign-in");
+        return;
+      }
+      const res = await fetch(`/api/comment/like-comment/${commentId}`, {
+        method: "PUT",
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setComments(
+          comments.map((comment) =>
+            comment._id === commentId
+              ? {
+                  ...comment,
+                  likes: data.likes,
+                  numberOfLikes: data.likes.length,
+                }
+              : comment
+          )
+        );        
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -97,35 +126,28 @@ export default function CommentSection({ postId }) {
               Submit
             </Button>
           </div>
-         {
-            commentError && 
-            <Alert color='failure' className="mt-5">
-            {commentError}
-          </Alert>
-         }
+          {commentError && (
+            <Alert color="failure" className="mt-5">
+              {commentError}
+            </Alert>
+          )}
         </form>
       )}
-      {
-        comments.length===0 ? (
-          <p className="text-sm my-5">No comments yet</p>
-        ):(
-          <>
+      {comments.length === 0 ? (
+        <p className="text-sm my-5">No comments yet</p>
+      ) : (
+        <>
           <div className="text-sm my-5 flex items-center gap-1">
             <p>Comments</p>
             <div className="border border-gray-500 rounded-sm py-1 px-2">
               <p>{comments.length}</p>
             </div>
           </div>
-          {
-            comments.map((comment)=>(
-              <Comment key={comment._id} comment={comment}/>
-            ))
-          }
-          </>
-
-          
-        )
-      }
+          {comments.map((comment) => (
+            <Comment key={comment._id} comment={comment} onLike={handleLike} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
